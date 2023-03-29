fd9ff179e3cca3b8bd5c5fbe0c3422346e6fa4e4
Baeyuyeon <dbdus983@naver.com>
2023-03-29 21:37:22 +0900

    Member update, delete 추가

--- src/main/java/com/board/toyproject/controller/MemberController.java
+++ src/main/java/com/board/toyproject/controller/MemberController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class MemberController {
@@ -19,13 +21,88 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
     @RequestMapping(value = "/create_member", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public Map<String, Object> createUser(@RequestBody Member member) {
+    public Map<String, Object> createMember(@RequestBody Member member) {
         System.out.println("member = " + member);
         memberService.join(member);
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("result", "added");
+        return resultMap;
+    }
+
+    /**
+     * 아이디로 멤버 찾기
+     * @param memberId
+     * @return
+     */
+    @GetMapping("/requestMemberById")
+    @ResponseBody
+    public Member requestMemberById(@RequestParam(value = "memberId",required = false) String memberId){
+        Member member = memberService.findByMemberId(memberId).orElse(null);
+        System.out.println("member = " + member);
+        return member;
+    }
+
+    /**
+     * 이름으로 멤버 찾기
+     * @param name
+     * @return
+     */
+    @GetMapping("/requestMemberByName")
+    @ResponseBody
+    public Member requestMemberByName(@RequestParam(value = "name",required = false) String name){
+        Member member = memberService.findByMemberName(name).orElse(null);
+        System.out.println("member = " + member);
+        return member;
+    }
+
+    /**
+     * 모든 멤버 조회하기
+     * @return
+     */
+    @GetMapping("/requestMemberAll")
+    @ResponseBody
+    public List<Member> requestUserAll(){
+        List<Member> memberList = memberService.findAllMember();
+        return memberList;
+    }
+
+    /**
+     * 멤버 정보 수정
+     * @param member
+     * @return
+     */
+    @RequestMapping(value = "/updateMember", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public Map<String, Object> updateMember(@RequestBody Member member) {
+        System.out.println("member = " + member);
+        Map<String, Object> resultMap = new HashMap<>();
+        if (member == null) {
+            resultMap.put("result", "멤버 정보를 입력하세요.");
+        }else{
+            String memberId=memberService.updateMember(member);
+            resultMap.put("result", memberId+" updated");
+        }
+        return resultMap;
+    }
+    /**
+     * 멤버 탈퇴
+     * @param member
+     * @return
+     */
+    @RequestMapping(value = "/deleteMember", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public Map<String, Object> deleteMember(@RequestBody Member member) {
+        System.out.println("member = " + member);
+        String memberId=memberService.deleteMember(member);
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("result", memberId+" deleted");
         return resultMap;
     }
 }
--- src/main/java/com/board/toyproject/service/MemberService.java
+++ src/main/java/com/board/toyproject/service/MemberService.java
@@ -4,6 +4,7 @@ import com.board.toyproject.domain.Member;
 import com.board.toyproject.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -13,26 +14,43 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
-    public String join(Member member){
+    public String join(Member member) {
         memberRepository.saveMember(member);
         return member.getMemberId();
     }
 
     /**
      * id로 멤버 검색
+     *
      * @param memberId
      * @return
      */
-    public Optional<Member> findByMemberId (String memberId){
+    public Optional<Member> findByMemberId(String memberId) {
         return memberRepository.findById(memberId);
     }
 
     /**
      * 이름으로 멤버 검색
+     *
      * @param name
      * @return
      */
-    public Optional<Member> findByMemberName(String name){
+    public Optional<Member> findByMemberName(String name) {
         return memberRepository.findByName(name);
+    }
+
+    public List<Member> findAllMember() {
+        return memberRepository.findAll();
+    }
+
+    //멤버 수정
+    public String updateMember(Member member) {
+        memberRepository.updateMember(member);
+        return member.getMemberId();
+    }
+    //멤버 탈퇴
+    public String deleteMember(Member member){
+        memberRepository.deleteMember(member);
+        return member.getMemberId();
     }
 }
