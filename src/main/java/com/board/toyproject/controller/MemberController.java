45339f8de0daaba2f1fb71200d6ff38c164eaaeb
Baeyuyeon <dbdus983@naver.com>
2023-04-09 22:14:29 +0900

    log4j 추가 중..

--- src/main/java/com/board/toyproject/controller/MemberController.java
+++ src/main/java/com/board/toyproject/controller/MemberController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 public class MemberController {
