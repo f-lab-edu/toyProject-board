2a912bf8048d1d3dcfc98dcc2e7838808fb95387
beayuyeon <dbdus983@naver.com>
2023-04-26 23:55:09 +0900

    젠킨스 PR 테스트8

--- src/test/java/com/board/toyproject/service/BoardServiceImplTest.java
+++ src/test/java/com/board/toyproject/service/BoardServiceImplTest.java
@@ -107,6 +107,7 @@ class BoardServiceImplTest {
         //when
         PageInfo<Board> boardList = PageInfo.of(boardService.findBoardBySearchWord(requestDTO));
         //then
+
         assertThat(boardList.getSize()).isEqualTo(2);
     }
     @Test
