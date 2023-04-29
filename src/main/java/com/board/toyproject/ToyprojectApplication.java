fd1c56edb6e8e4f5ab8255e1b68cbd64ea66806a
beayuyeon <dbdus983@naver.com>
2023-04-29 15:34:53 +0900

    jenkins 6

--- src/main/java/com/board/toyproject/ToyprojectApplication.java
+++ src/main/java/com/board/toyproject/ToyprojectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToyprojectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ToyprojectApplication.class, args);
 	}
