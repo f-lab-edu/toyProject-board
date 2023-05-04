2faa625d0582aa46216cef6b7c53f7dec9160165
beayuyeon <dbdus983@naver.com>
2023-05-04 09:04:30 +0900

    Pagination.java 에서 totalPageCount 제거

--- src/main/java/com/board/toyproject/domain/paging/Pagination.java
+++ src/main/java/com/board/toyproject/domain/paging/Pagination.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 public class Pagination<T> {
 
     private int totalRecordCount;     // 전체 데이터 수
-    private int totalPageCount;       // 전체 페이지 수
     private int limitStart;           // LIMIT 시작 위치
 
     private List<T> list = new ArrayList<>();
@@ -20,20 +19,11 @@ public class Pagination<T> {
     public Pagination(int totalRecordCount, PagingRequestData params) {
         if (totalRecordCount > 0) {
             this.totalRecordCount = totalRecordCount;
-            calculation(params);
+            calculate(params);
         }
     }
 
-    private void calculation(PagingRequestData params) {
-
-        // 전체 페이지 수 계산
-        totalPageCount = ((totalRecordCount - 1) / params.getRecordSize()) + 1;
-
-        // 현재 페이지 번호가 전체 페이지 수보다 큰 경우, 현재 페이지 번호에 전체 페이지 수 저장
-        if (params.getPage() > totalPageCount) {
-            params.setPage(totalPageCount);
-        }
-
+    private void calculate(PagingRequestData params) {
         // LIMIT 시작 위치 계산
         limitStart = (params.getPage() - 1) * params.getRecordSize();
     }
